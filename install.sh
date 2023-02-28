d1d2b3ff3ce5cd94c6c56ff3f08e0785fb0e1150
Povilas Krasavinas <povilas.krasavinas@nortal.com>
2023-02-28 11:31:28 +0200

    fix applications syntax, fix app-store setup

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Check if Homebrew is already installed
-if test ! $(which brew); then
+which -s brew
+
+if [[ $? != 0 ]] ; then
     echo 'Installing Homebrew'
   # Install Homebrew
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   
   # Add Homebrew to PATH
-#   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.bash_profile
-#   eval "$(/opt/homebrew/bin/brew shellenv)" 
+   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+   N
 else
   echo 'Homebrew already installed'
-
-  if test ! $(which ansible); then
+  
+  which -s ansible
+  if [[ $? != 0 ]] ; then
 
     echo 'Installing Ansible'
     
